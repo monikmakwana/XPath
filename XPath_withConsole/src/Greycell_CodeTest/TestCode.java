3046d33093980d87c0624b591fb5577869af623c
Monik Makwana <55778433+monikmakwana@users.noreply.github.com>
2020-10-21 01:37:24 +0530

    Add files via upload

---
+++ XPath_withConsole/src/Greycell_CodeTest/TestCode.java
@@ -0,0 +1,64 @@
+/*
+Program of XPath Processor
+By : Monik Makwana
+*/
+package Greycell_CodeTest;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpression;
+import javax.xml.xpath.XPathFactory;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import java.util.Scanner;
+import java.io.StringWriter;
+
+public class TestCode 
+{
+    public static void main(String[] args) throws Exception 
+    {
+		Scanner sc=new Scanner(System.in);
+		String expression,finalExpr;
+		
+		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+		DocumentBuilder builder = factory.newDocumentBuilder();
+        Document doc = builder.parse("F:/Eclipse_Workspace/XPath_withConsole/src/XMLfile.xml");
+	
+		
+		TransformerFactory transformerFactory = TransformerFactory.newInstance();
+		Transformer transformer = transformerFactory.newTransformer();
+		
+		transformer.setOutputProperty(OutputKeys.METHOD ,"xml");
+		transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+		transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "4");	
+		transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "yes");
+		
+		StringWriter sw = new StringWriter();
+        StreamResult result = new StreamResult(sw);
+		
+		XPathFactory xpathfactory = XPathFactory.newInstance();
+        XPath xpath = xpathfactory.newXPath();
+		
+		System.out.println();
+		System.out.print("expression : ");
+		expression=sc.next();
+		XPathExpression expr = xpath.compile(expression);
+		
+		Object resultt = expr.evaluate(doc, XPathConstants.NODESET);
+		NodeList nodes = (NodeList) resultt;	
+		System.out.println();
+		
+		for (int i = 0; i < nodes.getLength(); i++)
+		{
+			DOMSource source = new DOMSource(nodes.item(i));
+			transformer.transform(source, result);
+		}   
+		System.out.println(result.getWriter().toString());			
+	}
+}
